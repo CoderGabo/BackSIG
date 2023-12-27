4f16ec5f150e0264ef66b3b634dc57fb9e8ab51d
Gabriel <gabdihu@gmail.com>
2023-12-27 00:26:21 -0400

    conexion produccion

--- config/db.js
+++ config/db.js
@@ -1,30 +1,30 @@
 const {Sequelize} = require('sequelize');
 require('dotenv').config();
 
-// const sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//         host: process.env.DB_HOST,
-//         dialect: "postgres",
-//         timezone: process.env.DB_TZ,
-//         dialectOptions: {
-//             ssl: {
-//                 require: true,
-//             },
-//         },
-
-//     }
-// );
-
 const sequelize = new Sequelize(
-    'CentroComercial',
+    process.env.DB_NAME,
     'postgres',
-    'Restaurante',
+    process.env.DB_PASSWORD,
     {
-        host: 'localhost',
+        host: process.env.DB_HOST,
         dialect: "postgres",
+        // timezone: process.env.DB_TZ,
+        // dialectOptions: {
+        //     ssl: {
+        //         require: true,
+        //     },
+        // },
+
     }
 );
+
+// const sequelize = new Sequelize(
+//     'CentroComercial',
+//     'postgres',
+//     'Restaurante',
+//     {
+//         host: 'localhost',
+//         dialect: "postgres",
+//     }
+// );
 module.exports = { sequelize };
