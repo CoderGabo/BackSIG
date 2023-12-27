6a7f417c00695cd644d0c6a7fc3ad5f77e37f7a9
Gabriel <gabdihu@gmail.com>
2023-12-27 09:44:20 -0400

    consultas extras v0.2

--- config/db.js
+++ config/db.js
@@ -1,30 +1,30 @@
 const {Sequelize} = require('sequelize');
 require('dotenv').config();
 
-// const sequelize = new Sequelize(
-//     process.env.BD_NAME,
-//     process.env.BD_USER,
-//     process.env.BD_PASSWORD,
-//     {
-//         host: process.env.BD_HOST,
-//         dialect: "postgres",
-//         timezone: process.env.BD_TZ,
-//         dialectOptions: {
-//             ssl: {
-//                 require: true,
-//             },
-//         },
-
-//     }
-// );
-
 const sequelize = new Sequelize(
-    'CentroComercial',
-    'postgres',
-    'Restaurante',
+    process.env.BD_NAME,
+    process.env.BD_USER,
+    process.env.BD_PASSWORD,
     {
-        host: 'localhost',
+        host: process.env.BD_HOST,
         dialect: "postgres",
+        timezone: process.env.BD_TZ,
+        dialectOptions: {
+            ssl: {
+                require: true,
+            },
+        },
+
     }
 );
+
+// const sequelize = new Sequelize(
+//     'CentroComercial',
+//     'postgres',
+//     'Restaurante',
+//     {
+//         host: 'localhost',
+//         dialect: "postgres",
+//     }
+// );
 module.exports = { sequelize };
